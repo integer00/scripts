d9f58740a08a0714761ecae0b7eb42426d729986
integer <kiryxa00@gmail.com>
2018-09-14 18:41:39 +0000

    fix

---
+++ lfs/prepare_lfs_user.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+mdkir -v $LFS/sources
+mkdir -v $LFS/tools
+ln -sv $LFS/tools /
+
+groupadd lfs
+useradd -s /bin/bash -g lfs -m -k /dev/null lfs
+passwd lfs
+chown -v lfs $LFS/tools
+chown -v lfs $LFS/sources
+
+cat > /home/lfs/.bash_profile << "EOF"
+exec env -i HOME=$HOME TERM=$TERM PS1='\u:\w\$ ' /bin/bash
+EOF
+
+cat > /home/lfs/.bashrc << "EOF"
+set +h
+umask 022
+LFS=/mnt/lfs
+LC_ALL=POSIX
+LFS_TGT=$(uname -m)-lfs-linux-gnu
+PATH=/tools/bin:/bin:/usr/bin
+export LFS LC_ALL LFS_TGT PATH
+EOF
---
+++ lfs/wget-packages.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+wget --input-file=sources.txt --continue --directory-prefix=$LFS/sources
